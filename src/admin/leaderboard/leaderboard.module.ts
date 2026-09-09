caf43ecc2fa0cc9f648d8c77f1843b7454691463
Neaz Mahmood <neazmahmood445@gmail.com>
2026-09-09 13:38:03 +0600

    feat: implement public landing page controllers and leaderboard service endpoint

--- src/admin/leaderboard/leaderboard.module.ts
+++ src/admin/leaderboard/leaderboard.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { LeaderboardRecord } from './leaderboard-record.entity';
 import { LeaderboardController } from './leaderboard.controller';
+import { PublicLeaderboardController } from '../../public/public-leaderboard.controller';
 import { LeaderboardService } from './leaderboard.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([LeaderboardRecord])],
-  controllers: [LeaderboardController],
+  controllers: [LeaderboardController, PublicLeaderboardController],
   providers: [LeaderboardService],
   exports: [LeaderboardService, TypeOrmModule],
 })
